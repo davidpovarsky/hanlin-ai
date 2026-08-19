ba44b7593d685b1083b5c88096bce43dddc760af
davidpovarsky <davidpovarski1@gmail.com>
2026-08-19 19:01:56 +0300

    Fix TranslationUIProvider main actor isolation

--- HanlinTranslationExtension/HanlinTranslationExtension.swift
+++ HanlinTranslationExtension/HanlinTranslationExtension.swift
@@ -2,6 +2,7 @@ import SwiftUI
 import TranslationUIProvider
 
 @main
+@MainActor
 final class HanlinTranslationExtension: TranslationUIProviderExtension {
     var body: some TranslationUIProviderExtensionScene {
         TranslationUIProviderSelectedTextScene { context in
